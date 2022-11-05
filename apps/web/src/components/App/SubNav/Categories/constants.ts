765b3dd7147da41b9fae2b27446f54f35b7b7a75
Damian Kocjan <DamianoKPL@wp.pl>
2022-11-05 23:05:41 +0100

    Create constants.ts

---
+++ apps/web/src/components/App/SubNav/Categories/constants.ts
@@ -0,0 +1,271 @@
+import { CategoryItemProps as DesktopCategoryItemProps } from "./Desktop";
+import { CategoryItemProps as MobileCategoryItemProps } from "./Mobile";
+
+export const MOBILE_CATEGORIES: MobileCategoryItemProps[] = [
+	{ name: "New", href: "/c/new" },
+	{ name: "Clothes", href: "/c/clothes" },
+	{ name: "Footwear", href: "/c/footwear" },
+	{ name: "Sport", href: "/c/sport" },
+	{ name: "Accessories", href: "/c/accessories" },
+	{ name: "Brands", href: "/c/brands" },
+	{ name: "Sale", href: "/c/sale" },
+];
+
+export const DESKTOP_CATEGORIES: DesktopCategoryItemProps[] = [
+	{
+		name: "New",
+		href: "/c/new",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [
+					{
+						name: "Clothes",
+						href: "/clothes",
+					},
+					{
+						name: "Footwear",
+						href: "/footwear",
+					},
+					{
+						name: "Accessories",
+						href: "/accessories",
+					},
+					{
+						name: "Dresses",
+						href: "/dresses",
+					},
+					{
+						name: "T-shirts and tops",
+						href: "/t-shirts-and-tops",
+					},
+					{
+						name: "Pants",
+						href: "/pants",
+					},
+					{
+						name: "Sandals and flip-flops",
+						href: "/sandals-and-flip-flops",
+					},
+					{
+						name: "Bags and backpacks",
+						href: "/bags-and-backpacks",
+					},
+					{
+						name: "Sport",
+						href: "/sport",
+					},
+					{
+						name: "Beauty",
+						href: "/beauty",
+					},
+				],
+			},
+			{
+				subtitle: "Men",
+				categories: [
+					{
+						name: "Clothes",
+						href: "/clothes",
+					},
+					{
+						name: "Footwear",
+						href: "/footwear",
+					},
+					{
+						name: "Accessories",
+						href: "/accessories",
+					},
+					{
+						name: "T-shirts",
+						href: "/t-shirts",
+					},
+					{
+						name: "Sweatshirts and sweaters",
+						href: "/sweatshirts-and-sweaters",
+					},
+					{
+						name: "Pants",
+						href: "/pants",
+					},
+					{
+						name: "Sneakers",
+						href: "/sneakers",
+					},
+					{
+						name: "Summer shoes",
+						href: "/summer-shoes",
+					},
+					{
+						name: "Sport",
+						href: "/sport",
+					},
+					{
+						name: "Cosmetics",
+						href: "/cosmetics",
+					},
+				],
+			},
+			{
+				subtitle: "Kids",
+				categories: [
+					{
+						name: "Clothes",
+						href: "/clothes",
+					},
+					{
+						name: "Footwear",
+						href: "/footwear",
+					},
+					{
+						name: "Accessories",
+						href: "/accessories",
+					},
+					{
+						name: "T-shirts",
+						href: "/t-shirts",
+					},
+					{
+						name: "Sweatshirts and sweaters",
+						href: "/sweatshirts-and-sweaters",
+					},
+					{
+						name: "Pants",
+						href: "/pants",
+					},
+					{
+						name: "Sneakers",
+						href: "/sneakers",
+					},
+					{
+						name: "Summer shoes",
+						href: "/summer-shoes",
+					},
+					{
+						name: "Sport",
+						href: "/sport",
+					},
+					{
+						name: "Cosmetics",
+						href: "/cosmetics",
+					},
+				],
+			},
+		],
+	},
+	{
+		name: "Clothes",
+		href: "/c/clothes",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+	{
+		name: "Footwear",
+		href: "/c/footwear",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+	{
+		name: "Sport",
+		href: "/c/sport",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+	{
+		name: "Accessories",
+		href: "/c/accessories",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+	{
+		name: "Brands",
+		href: "/c/brands",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+	{
+		name: "Sale",
+		href: "/c/sale",
+		image: "https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		columns: [
+			{
+				subtitle: "Women",
+				categories: [],
+			},
+			{
+				subtitle: "Men",
+				categories: [],
+			},
+			{
+				subtitle: "Kids",
+				categories: [],
+			},
+		],
+	},
+];
