b801b7736864b6d5e3b14aef878dbae84523ccda
Damian Kocjan <DamianoKPL@wp.pl>
2022-12-20 21:39:14 +0100

    feat: title text underline on `For who` card hover

--- apps/web/src/components/Home/ForWho.tsx
+++ apps/web/src/components/Home/ForWho.tsx
@@ -2,6 +2,27 @@ import { Grid } from "@ecommerce/ui";
 import Link from "next/link";
 import React from "react";
 
+const FOR_WHO = [
+	{
+		title: "For Women",
+		imageSrc:
+			"https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		href: "/c/women",
+	},
+	{
+		title: "For Men",
+		imageSrc:
+			"https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		href: "/c/men",
+	},
+	{
+		title: "For Kids",
+		imageSrc:
+			"https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg",
+		href: "/c/kids",
+	},
+] as const;
+
 export const ForWho: React.FC = () => {
 	return (
 		<section className="mt-16">
@@ -15,49 +36,25 @@ export const ForWho: React.FC = () => {
 				justify="center"
 				className="p-4 gap-4 w-full mt-2 sm:grid-cols-2 lg:grid-cols-3"
 			>
-				<Link href="/c/women">
-					<a className="group bg-black p-4">
-						<article>
-							<img
-								src="https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg"
-								alt="women"
-								className="object-cover grayscale group-hover:grayscale-0 transition-filter"
-							/>
-							<h4 className="text-4xl font-bold font-mono text-white py-2">
-								For Women
-							</h4>
-						</article>
-					</a>
-				</Link>
-				<Link href="/c/men">
-					<a className="group bg-black p-4">
-						<article>
-							<img
-								src="https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg"
-								alt="men"
-								className="object-cover grayscale group-hover:grayscale-0 transition-filter"
-							/>
-							<h4 className="text-4xl font-bold font-mono text-white py-2">
-								Men
-							</h4>
-						</article>
-					</a>
-				</Link>
-				<Link href="/c/kids">
-					<a className="group bg-black p-4 sm:col-span-2 sm:place-self-center lg:col-span-1">
-						<article>
-							<img
-								src="https://tailwindcss.com/_next/static/media/retro-shoe.24e25785.jpg"
-								alt="kids"
-								// TODO: fix max height
-								className="sm:max-h-96 md:max-h-fit object-cover grayscale group-hover:grayscale-0 transition-filter"
-							/>
-							<h4 className="text-4xl font-bold font-mono text-white py-2">
-								Kids
-							</h4>
-						</article>
-					</a>
-				</Link>
+				{/* FIXME: fix last card max height on smaller screens */}
+				{/* FIXME: fix first two width on smaller screens */}
+				{FOR_WHO.map(({ title, imageSrc, href }) => (
+					<Link href={href}>
+						<a className="group bg-black p-4 last-of-type:sm:col-span-2 last-of-type:sm:place-self-center last-of-type:lg:col-span-1">
+							<article>
+								<img
+									src={imageSrc}
+									alt="women"
+									className="object-cover grayscale group-hover:grayscale-0 transition-filter last-of-type:sm:max-h-96 last-of-type:md:max-h-fit"
+								/>
+								<h4 className="text-4xl font-bold font-mono text-white py-2 relative w-fit">
+									{title}
+									<span className="absolute bottom-0 left-4 hidden group-hover:block w-full h-1 bg-white" />
+								</h4>
+							</article>
+						</a>
+					</Link>
+				))}
 			</Grid>
 		</section>
 	);
