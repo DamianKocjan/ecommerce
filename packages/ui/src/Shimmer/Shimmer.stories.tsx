ff628acf4449b359e344c7b622b2d1081f45a513
Damian Kocjan <DamianoKPL@wp.pl>
2022-12-23 02:06:26 +0100

    docs: create example storybook for `Shimmer` component

--- packages/ui/src/Shimmer/Shimmer.stories.tsx
+++ packages/ui/src/Shimmer/Shimmer.stories.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { ComponentMeta } from "@storybook/react";
-
+import { Flex } from "../Flex";
 import { Shimmer } from "./Shimmer";
 
 export default {
@@ -8,4 +7,11 @@ export default {
 	component: Shimmer,
 } as ComponentMeta<typeof Shimmer>;
 
-export const Template = () => <Shimmer />;
+export const Template = () => (
+	<Flex className="gap-4" direction="col">
+		<Shimmer shape="circle" className="w-40 h-40" />
+		<Shimmer shape="rectangular" className="w-1/2 h-20" />
+		<Shimmer shape="rounded" className="w-1/2 h-4" />
+		<Shimmer shape="text" className="w-1/2 h-4" />
+	</Flex>
+);
