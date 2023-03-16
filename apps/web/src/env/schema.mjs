e1625dbaf6883b212fd26aabf31c4181536ad96c
Damian Kocjan <DamianoKPL@wp.pl>
2023-03-16 01:47:30 +0100

    fix: update env schema

--- apps/web/src/env/schema.mjs
+++ apps/web/src/env/schema.mjs
@@ -14,6 +14,8 @@ export const serverSchema = z.object({
 	DATABASE_URL: z.string(),
 	AWS_S3_IAM_ACCESS: z.string(),
 	AWS_S3_IAM_SECRET: z.string(),
+	SIMPLE_ANALYTICS_USER_ID: z.string(),
+	SIMPLE_ANALYTICS_API_KEY: z.string(),
 });
 
 /**
@@ -22,7 +24,6 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-	NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().optional(),
 	// NEXT_PUBLIC_BAR: z.string(),
 });
 
@@ -33,6 +34,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-	NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
 	// NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
 };
