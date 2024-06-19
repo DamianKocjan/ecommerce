b4458a77dbd4d64a5cf2ca755ec7432e80c4c462
Damian Kocjan <DamianoKPL@wp.pl>
2024-06-19 13:13:41 +0200

    chore: remove old files

--- apps/new_web/src/components/shared/hooks/useNumberFormatter.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from "react";
-
-export function useNumberFormatter(locale = "en-us") {
-	const formatter = useMemo(() => new Intl.NumberFormat(locale), [locale]);
-
-	return formatter;
-}
