b66f807fecc6fc9442664f6afb4c1621a4ab65ee
Damian Kocjan <DamianoKPL@wp.pl>
2024-06-19 13:08:02 +0200

    feat: seed fake manufacturers

--- packages/db/prisma/seed/manufacturers.ts
+++ packages/db/prisma/seed/manufacturers.ts
@@ -1,34 +1,25 @@
+import { faker } from "@faker-js/faker";
 import type { Prisma, PrismaClient } from "../..";
 
-const MANUFACTURERS: Prisma.ManufacturerCreateInput[] = [
-	{
-		name: "Nike",
-		description:
-			"Nike is an American multinational corporation that is engaged in the design, development, manufacturing, and worldwide marketing and sales of footwear, apparel, equipment, accessories, and services.",
-		country: "USA",
-		headquarters: "Beaverton, Oregon, U.S.",
-		yearOfFoundation: 1964,
-	},
-	{
-		name: "Adidas",
-		description:
-			"Adidas AG is a German multinational corporation that manufactures and markets sports shoes, apparel, and equipment.",
-		country: "Germany",
-		headquarters: "Herzogenaurach, Bavaria, Germany",
-		yearOfFoundation: 1949,
-	},
-	{
-		name: "Tommy Hilfiger",
-		description:
-			"Tommy Hilfiger B.V. formerly known as Tommy Hilfiger Corporation and Tommy Hilfiger Inc., is an American premium clothing brand, manufacturing apparel, footwear, accessories, fragrances and home furnishings",
-		country: "Netherlands",
-		headquarters: "Amsterdam, Netherlands",
-		yearOfFoundation: 1985,
-	},
-];
+const NUMBER_OF_MANUFACTURERS = 20;
 
 export async function seedManufacturers(prisma: PrismaClient): Promise<void> {
 	await prisma.manufacturer.createMany({
-		data: MANUFACTURERS,
+		data: generateManufacturers(),
 	});
+}
+
+function generateManufacturers() {
+	const manufacturers: Prisma.ManufacturerCreateInput[] = Array.from(
+		{ length: NUMBER_OF_MANUFACTURERS },
+		() => ({
+			name: faker.company.name(),
+			description: faker.lorem.paragraph(),
+			country: faker.location.country(),
+			headquarters: faker.location.city(),
+			yearOfFoundation: faker.date.past().getFullYear(),
+		}),
+	);
+
+	return manufacturers;
 }
