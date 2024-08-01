1bea9fe335de48c7884f5236dd3dd8ac2cb116fd
Damian Kocjan <DamianoKPL@wp.pl>
2024-08-01 12:43:05 +0200

    feat: add button with text variant for wishlist button

--- apps/new_web/src/components/shop/wishlist-button.tsx
+++ apps/new_web/src/components/shop/wishlist-button.tsx
@@ -4,7 +4,11 @@ import React from "react";
 import { useWishlist } from "~/hooks/use-wishlist";
 import { Button } from "../ui/button";
 
-export function WishlistButton({ productSkuId }: { productSkuId: number }) {
+type Props = {
+	productSkuId: number;
+};
+
+export function WishlistIconButton({ productSkuId }: Props) {
 	const { handleToggleWishlist, isInWishlist } = useWishlist(productSkuId);
 	const [additionalClasses, setAdditionalClasses] = React.useState("");
 
@@ -51,6 +55,55 @@ export function WishlistButton({ productSkuId }: { productSkuId: number }) {
 					aria-hidden="true"
 				/>
 			)}
+		</Button>
+	);
+}
+
+export function WishlistButton({ productSkuId }: Props) {
+	const { handleToggleWishlist, isInWishlist } = useWishlist(productSkuId);
+	const [additionalClasses, setAdditionalClasses] = React.useState("");
+
+	const handleMouseUp = React.useCallback(() => {
+		if (!isInWishlist) {
+			return;
+		}
+
+		setAdditionalClasses("animate-upShake");
+
+		setTimeout(() => {
+			setAdditionalClasses("");
+		}, 600);
+	}, [isInWishlist]);
+
+	return (
+		<Button
+			variant="outline"
+			type="button"
+			className="group"
+			onClick={handleToggleWishlist}
+			onMouseUp={handleMouseUp}
+		>
+			{isInWishlist ? (
+				<>
+					<HeartBreak
+						className={`mr-2 hidden h-4 w-4 hover:animate-wiggle group-hover:block ${additionalClasses}`}
+						aria-hidden="true"
+						weight="fill"
+					/>
+					<Heart
+						className={`mr-2 h-4 w-4 hover:animate-wiggle group-hover:hidden ${additionalClasses}`}
+						aria-hidden="true"
+						weight="fill"
+					/>
+				</>
+			) : (
+				<Heart
+					className={`mr-2 h-4 w-4 hover:animate-wiggle ${additionalClasses}`}
+					aria-hidden="true"
+				/>
+			)}
+
+			{isInWishlist ? "Remove from wishlist" : "Add to wishlist"}
 		</Button>
 	);
 }
